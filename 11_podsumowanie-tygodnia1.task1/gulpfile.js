037a4b638e5f4735def1a30b0e1b9fb8d92122eb
Kamil Cholys <kamilcholys@gmail.com>
2019-12-17 10:18:46 +0100

    11_podsumowanie solved

--- 11_podsumowanie-tygodnia1.task1/gulpfile.js
+++ 11_podsumowanie-tygodnia1.task1/gulpfile.js
@@ -1,33 +1,55 @@
-const gulp = require('gulp');
+
+const gulp = require("gulp");
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
-const c = require('ansi-colors');
+const colors = require('ansi-colors');
+const notifier = require('node-notifier');
+const browserSync = require('browser-sync').create();
 
-//własna funkcja pokazująca błędy
-function ourErrors(err) {
-  console.log(c.red("----------------"));
-  //by zobaczyć z jakich właściwości możemy skorzystać możemy w konsoli wypisać err.toString()
-  console.log(c.red(err.messageFormatted));
-  console.log(c.red("----------------"));
-  this.emit("end");
+const showError = function(err){
+    //console.log(err.toString());
+
+    notifier.notify({
+        title: 'Error in sass',
+        message: err.messageFormated
+    })
+
+    console.log(colors.red('======================'));
+    console.log(colors.red(err.messageFormatted));
+    console.log(colors.red('======================'));
+
 }
 
-function compileSass(cb) {
-    gulp.src('./scss/main.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle : "expanded"}).on('error', ourErrors)) //własne błędy
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./css'));
+const server = function(cb){
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+        notify: false,
+        open: true
+    });
+
     cb();
 }
 
-function watcher(cb) {
-  gulp.watch('./scss/**/*.scss', gulp.series(compileSass));
+const css = function() {
+    return gulp.src('scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: "extended"
+        })).on("error", showError)
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest('dist/css')) // folder destynacji gdzie ma stworzyć plik css
+        .pipe(browserSync.stream({match:'**/*.css'}));
 }
 
-exports.default = gulp.parallel(compileSass, watcher);
+const watch = function () {
+    gulp.watch('scss/**/*.scss', gulp.series(css));
+    gulp.watch('*.html').on("change", browserSync.reload);
+}
+
+
+exports.default = gulp.series(css, server, watch);
+exports.css = css;
